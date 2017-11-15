1fe3a98211a12fe08267abb8208cb4f7dd550adb
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-15 12:58:10 -0500

    ->Views changes

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /polls/
 	url(r'^$', views.index, name="index"),
+
+	# polls/5
+	url(r'^$')
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello world this is the index for the polls.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question $s."%question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of the question %s"
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s"%question_id)
