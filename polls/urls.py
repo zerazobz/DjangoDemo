d9601fd9854372610516a19d44602a12e5426e0c
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-15 18:25:05 -0500

    Se usan plantillas para las vistas

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
 	url(r'^$', views.index, name="index"),
 
 	# polls/5
-	url(r'^$')
+	# url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+	#polls/5/results
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+
+	#polls/5/vote
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world this is the index for the polls.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    # return HttpResponse(output)
+	# return HttpResponse("Hello world this is the index for the polls.")
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at the question $s."%question_id)
+	return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of the question %s"
