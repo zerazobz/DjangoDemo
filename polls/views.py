a2205832025c698a7d7803f98d151141c90ba498
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-15 19:38:03 -0500

    ->formulario funcional

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.http import Http404
+
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -13,17 +15,23 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk = question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exists")
-    # return render(request, "polls/detail.html", {'question': question})
     question = get_object_or_404(Question, pk = question_id)
     return  render(request, "polls/detail.html", {'question' : question})
 
 def results(request, question_id):
-    response = "You're looking at the results of the question %s"
-    return HttpResponse(response%question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, "polls/results.html", { 'question' : question })
+    # response = "You're looking at the results of the question %s"
+    # return HttpResponse(response%question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s"%question_id)
+    question = Question.objects.get(pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", { 'question': question, 'error_message': "You didn't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id, )))
+    # return HttpResponse("You're voting on question %s"%question_id)
